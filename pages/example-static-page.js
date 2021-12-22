e7af7d88ab7a2d46d5f5756da586c8d184ecbcd6
Caleb Lawrence <lawrence.calebc@gmail.com>
2021-12-22 09:30:49 -0600

    updated example

---
+++ pages/example-static-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ISRTest = ({ time }) => {
+  return (
+    <div style={{ margin: 50 }}>
+      <p style={{ fontSize: 30 }}>Static Page</p>
+    </div>
+  );
+};
+
+export default ISRTest;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const ISRTest = ({ time }) => {
- 
   return (
-      <div style={{margin: 50}}>
-            <p style={{fontSize: 30}}>{time}</p>
-      </div>
+    <div style={{ margin: 50 }}>
+      <p style={{ fontSize: 30 }}>{time}</p>
+      <Link href="example-static-page">Link to static page</Link>
+    </div>
   );
 };
 
 export async function getStaticProps() {
-  let currentTime = new Date()
-  currentTime.toLocaleString('en-US', { timeZone: 'America/Chicago' })
+  let currentTime = new Date();
+  currentTime.toLocaleString("en-US", { timeZone: "America/Chicago" });
 
   console.log("currentTime", currentTime);
   return {
